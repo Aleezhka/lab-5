50693f28bb2af9632f694901e95fe1b96742e3fd
Aleezhka <151876861+Aleezhka@users.noreply.github.com>
2023-11-29 18:08:15 +0200

    I deleted redundant library and adding class docstrings

--- Lab5.py
+++ Lab5.py
@@ -1,30 +1,55 @@
+"""
+Створити клас Clothing (name, description, location, colour, size). Додати enum type: SHIRT, JEANS,
+JACKET, ….Додати клас Wardrobe, котрий містить одяг.Визначити функцію are goOut(Clothing*) - вивести
+кількість типів одягу та чи готова людина вийти на вулицю (якщо кількість типів більша, ніж
+три - так, менша - ні). Посортуйте одяг за розміром. Додайте ще один метод до класу одягу.
+"""
 from enum import Enum
-import pytest
+
 
 class ClothingType(Enum):
+    """
+    Enumeration representing different types of clothing
+    """
     SHIRT = "shirt"
     JEANS = "jeans"
     JACKET = "jacket"
 
 
 class Clothing:
-
-    def __init__(self, name, description, location, colour, size, clothing_type):
+    """
+     Represents an individual piece of clothing
+    """
+    def __init__(self, name, description, location, *args):
         """
         Initializing
         """
         self.__name = name
         self.__description = description
         self.__location = location
-        self.__colour = colour
-        self.size = size
-        self.clothing_type = clothing_type
+        self.__colour = args[0]
+        self.__size = args[1]
+        self.__clothing_type = args[2]
 
     def __del__(self):
         """
         Destructed clothing
         """
         return f"Clothing {self.__name} has been destructed"
+
+    @property
+    def clothing_type(self):
+        """
+        Getter method for the clothing_type property
+        """
+        return self.__clothing_type
+
+    @property
+    def size(self):
+        """
+        Getter method for the size property
+        """
+        return self.__size
 
     def clothes_info(self):
         """
@@ -39,7 +64,9 @@ class Clothing:
 
 
 class Wardrobe:
-
+    """
+    Represents a collection of clothing items
+    """
     def __init__(self):
         """
         Initializing
@@ -54,7 +81,8 @@ class Wardrobe:
 
     def are_go_out(self):
         """
-        The method displays the number of clothes in the wardrobe and indicates the readiness of a person to go outside
+        The method displays the number of clothes in the wardrobe
+        and indicates the readiness of a person to go outside
         """
         unique_types = set()
         unique = []
@@ -80,21 +108,30 @@ class Wardrobe:
 
 
 if __name__ == "__main__":
-    shirt_1 = Clothing("T-Shirt", "Casual cotton shirt", "Wardrobe", "Blue", 33, ClothingType.SHIRT)
-    jeans_1 = Clothing("Blue Jeans", "Classic denim jeans", "Wardrobe", "Blue", 32, ClothingType.JEANS)
-    jacket_1 = Clothing("Leather Jacket", "Stylish leather jacket", "Wardrobe", "Black", 31, ClothingType.JACKET)
-    shirt_2 = Clothing("T-Shirt 2", "Casual cotton shirt", "Wardrobe", "Blue", 34, ClothingType.SHIRT)
-    jeans_2 = Clothing("White Jeans", "Classic denim jeans", "Wardrobe", "Blue", 37, ClothingType.JEANS)
-    jacket_2 = Clothing("Denim Jacket", "Stylish leather jacket", "Wardrobe", "Black", 31, ClothingType.JACKET)
-    shirt_3 = Clothing("T-Shirt 3", "Casual cotton shirt", "Wardrobe", "Blue", 33, ClothingType.SHIRT)
-    jeans_3 = Clothing("Black Jeans", "Classic denim jeans", "Wardrobe", "Blue", 32, ClothingType.JEANS)
-    jacket_3 = Clothing("Suede Jacket", "Stylish leather jacket", "Wardrobe", "Black", 31, ClothingType.JACKET)
+    shirt_1 = Clothing("T-Shirt", "Casual cotton shirt",
+                       "Wardrobe", "Blue", 33, ClothingType.SHIRT)
+    jeans_1 = Clothing("Blue Jeans", "Classic denim jeans",
+                       "Wardrobe", "Blue", 32, ClothingType.JEANS)
+    jacket_1 = Clothing("Leather Jacket", "Stylish leather jacket",
+                        "Wardrobe", "Black", 31, ClothingType.JACKET)
+    shirt_2 = Clothing("T-Shirt 2", "Casual cotton shirt",
+                       "Wardrobe", "Blue", 34, ClothingType.SHIRT)
+    jeans_2 = Clothing("White Jeans", "Classic denim jeans",
+                       "Wardrobe", "Blue", 37, ClothingType.JEANS)
+    jacket_2 = Clothing("Denim Jacket", "Stylish leather jacket",
+                        "Wardrobe", "Black", 31, ClothingType.JACKET)
+    shirt_3 = Clothing("T-Shirt 3", "Casual cotton shirt",
+                       "Wardrobe", "Blue", 33, ClothingType.SHIRT)
+    jeans_3 = Clothing("Black Jeans", "Classic denim jeans",
+                       "Wardrobe", "Blue", 32, ClothingType.JEANS)
+    jacket_3 = Clothing("Suede Jacket", "Stylish leather jacket",
+                        "Wardrobe", "Black", 31, ClothingType.JACKET)
 
     wardrobe = Wardrobe()
-    for cloth in [shirt_1, shirt_2, shirt_3, jeans_1, jeans_2, jeans_3, jacket_1, jacket_2, jacket_3]:
+    for cloth in [shirt_1, shirt_2, shirt_3, jeans_1, jeans_2,
+                  jeans_3, jacket_1, jacket_2, jacket_3]:
         wardrobe.add_clothes(cloth)
 
     wardrobe.sort_clothes()
 
     wardrobe.are_go_out()
-
